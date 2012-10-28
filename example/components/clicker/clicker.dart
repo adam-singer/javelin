659273773dccae62fb0d11a8b423ca6a4877722e
John McCutchan <john@johnmccutchan.com>
2012-10-28 14:48:14 -0700

    Use new import syntax.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- example/components/clicker/clicker.dart
+++ example/components/clicker/clicker.dart
@@ -1,18 +1,16 @@
+library javelin_click_demo;
 
-#library("javelin_clicker_demo");
+import 'dart:html';
+import 'package:vector_math/vector_math_browser.dart';
+import 'package:javelin/javelin.dart';
+import 'package:javelin/javelin_game.dart';
 
-#import('dart:isolate');
+part 'enemy.dart';
+part 'evade_mouse.dart';
+part 'destroy_on_click.dart';
+part 'score_printer.dart';
 
-#import('package:vector_math/vector_math_browser.dart');
-#import('package:javelin/javelin.dart');
-#import('package:javelin/javelin_game.dart');
-
-#source('enemy.dart');
-#source('evade_mouse.dart');
-#source('destroy_on_click.dart');
-#source('score_printer.dart');
-
-class Clicker extends Scene{
+class Clicker extends Scene {
 
   GameObject _scoreManager;
 
@@ -28,8 +26,13 @@ class Clicker extends Scene{
     }
 
     // Also add a vanilla GameObject and manually add a component to it:
-    _scoreManager = new GameObject();
+    _scoreManager = new GameObject('scoreManager');
     _scoreManager.attachComponent('ScorePrinter');
     root.addChild(_scoreManager);
   }
+}
+
+void main() {
+  Clicker clickerGame = new Clicker();
+  clickerGame.getGameObjectWithId('scoreManager');
 }
