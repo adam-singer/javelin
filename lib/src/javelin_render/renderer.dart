872f87f637e033576fca5314a4b752f16b248f29
John McCutchan <john@johnmccutchan.com>
2012-12-01 18:16:10 -0800

    Fullscreen layers working.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/src/javelin_render/renderer.dart
+++ lib/src/javelin_render/renderer.dart
@@ -2,6 +2,7 @@ part of javelin_render;
 
 class Renderer {
   final GraphicsDevice device;
+  final CanvasElement frontBuffer;
   final Map<String, Mesh> meshes = new Map<String, Mesh>();
   final Map<String, Shader> shaders = new Map<String, Shader>();
   final Map<String, Material> materials = new Map<String, Material>();
@@ -10,6 +11,7 @@ class Renderer {
   Viewport frontBufferViewport;
   GlobalResources globalResources;
   LayerConfig layerConfig;
+  SamplerState _npotSampler;
 
   void applyCameraUniforms() {
     // Walk over shaders
@@ -33,28 +35,88 @@ class Renderer {
   int _sortDrawables(List<Drawable> visibleSet,
                      int numVisible,
                      String layerName) {
+    return numVisible;
     int drawableCount = 0;
     for (int i = 0; i< numVisible; i++) {
-
     }
     drawableCount = 0;
   }
 
+  void _renderPassLayer(Layer layer, List<Drawable> drawables, Camera camera,
+                        Viewport viewport) {
+    // TODO: Sort according to layer
+    /*
+    for (int drawableIndex = 0;
+        drawableIndex < visibleCount;
+        drawableIndex++) {
+      Drawable drawable = visibleSet[drawableIndex];
+      // Apply material settings
+      // Bind mesh
+      // Draw
+    }
+    */
+  }
+
+  void _renderFullscreenLayer(Layer layer, List<Drawable> drawables,
+                              Camera camera, Viewport viewport) {
+    String process = layer.properties['process'];
+    String source = layer.properties['source'];
+    if (process != null && source != null) {
+      // TODO(johnmccutchan): Support better arugments.
+      Texture2D colorTexture = globalResources.findColorTarget(source);
+      Map arguments = {
+                       'textures': [colorTexture],
+                       'samplers': [_npotSampler],
+      };
+      SpectrePost.pass(process, layer.renderTarget, arguments);
+    }
+  }
+
   void render(List<Drawable> drawables, Camera camera, Viewport viewport) {
-    return;
-    // Determine list of drawables visible from camera.
+    device.configureDeviceChild(frontBufferViewport, {
+      'width': frontBuffer.width,
+      'height': frontBuffer.height,
+    });
+    device.context.setViewport(viewport);
     List<Drawable> visibleSet = new List<Drawable>(drawables.length);
     int visibleCount = 0;
     visibleCount = _updateVisibleSet(visibleSet, drawables, camera);
-    // Foreach layer:
-      // Apply layer target settings.
-      // Sort visibleSet accordingy to layer configuration.
-      // Foreach drawable:
-        // Draw.
+    int numLayers = layerConfig.layers.length;
+    for (int layerIndex = 0; layerIndex < numLayers; layerIndex++) {
+      Layer layer = layerConfig.layers[layerIndex];
+      device.context.setRenderTarget(layer.renderTarget);
+      if (layer.clearColor == true) {
+        num r = layer.clearColorR;
+        num g = layer.clearColorG;
+        num b = layer.clearColorB;
+        num a = layer.clearColorA;
+        device.context.clearColorBuffer(r, g, b, a);
+      }
+      if (layer.clearDepth == true) {
+        num v = layer.clearDepthValue;
+        device.context.clearDepthBuffer(v);
+      }
+      if (layer.type == 'pass') {
+        _renderPassLayer(layer, drawables, camera, viewport);
+      } else if (layer.type == 'fullscreen') {
+        _renderFullscreenLayer(layer, drawables, camera, viewport);
+      }
+    }
   }
 
-  Renderer(CanvasElement frontBuffer, this.device) {
+  Renderer(this.frontBuffer, this.device) {
     globalResources = new GlobalResources(this, frontBuffer);
     layerConfig = new LayerConfig(this);
+    SpectrePost.init(device);
+    _npotSampler = device.createSamplerState('_npotSampler', {
+      'wrapS': SamplerState.TextureWrapClampToEdge,
+      'wrapT': SamplerState.TextureWrapClampToEdge,
+      'minFilter': SamplerState.TextureMinFilterNearest,
+      'magFilter': SamplerState.TextureMagFilterNearest,
+    });
+    frontBufferViewport = device.createViewport('Renderer.Viewport', {
+      'width': frontBuffer.width,
+      'height': frontBuffer.height,
+    });
   }
 }
